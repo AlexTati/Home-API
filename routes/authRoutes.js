c5aadcc6d853765986f49b079a0f277544a84775
s4my <samseche@gmail.com>
2019-11-08 11:50:19 +0100

    bugfix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,7 @@ const authAction = require("../controllers/authController");
 module.exports = function (app, upload) {
 
     app.route("/auth/login")
-        .post(authAction.login , upload.none);
-
+        .post(upload.none(), authAction.login);
 
 }
 
--- routes/typesRoutes.js
+++ routes/typesRoutes.js
@@ -1,7 +1,5 @@
 const typesAction = require("../controllers/houseTypeController");
 
-
-
 module.exports = function (app, upload) {
     app.route('/house-types')
         .get(typesAction.getAll)
